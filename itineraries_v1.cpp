1822e29b39baa1c787ae6f0a4a3380e109c63bd0
czartur <arturalves.ca@gmail.com>
2023-01-08 22:02:03 +0100

    global variables

--- itineraries_v1.cpp
+++ itineraries_v1.cpp
@@ -5,9 +5,16 @@
 #include "MST.h"
 using namespace std;
 
+const int N = 2e5+5;
+const int M = 3e5+5;
+const int L = 5e5+5;
+
+int n, m, l; 
+vector<pair<int,int>> adjList[N];
+
 // compute max noise level (BFS approach)
-int query(int u, int v, vector<vector<pair<int,int>>> &adj){
-    vector<int> vis((int) adj.size());
+int query(int u, int v){
+    vector<int> vis(n);
     queue <pair<int,int>> q;
     q.push({u,0});
 
@@ -15,12 +22,12 @@ int query(int u, int v, vector<vector<pair<int,int>>> &adj){
         int u = q.front().first;
         int max_noise = q.front().second;
         q.pop();
-
+        
         if(u == v) return max_noise;
 
-        for(int i=0; i< (int) adj[u].size(); i++){
-            int cur_u = adj[u][i].first;
-            int cur_weight = adj[u][i].second;
+        for(int i=0; i< (int) adjList[u].size(); i++){
+            int cur_u = adjList[u][i].first;
+            int cur_weight = adjList[u][i].second;
             if(!vis[cur_u]) q.push({cur_u, max(max_noise, cur_weight)});
         }
     }
@@ -29,24 +36,23 @@ int query(int u, int v, vector<vector<pair<int,int>>> &adj){
 }
 
 int main(){
-    int n, m, l;
-    cin >> n >> m;
     vector<Edge> edgeList;
+    cin >> n >> m;
     while(m--){
         int u, v, c;
         cin >> u >> v >> c;
         edgeList.push_back(Edge(u-1, v-1, c)); 
     }
-
-    edgeList = build_MST(edgeList, n);
-    vector<vector<pair<int,int>>> adjList = to_adj_list(edgeList, n);
-
+    build_MST(adjList, edgeList, n);
+    
+    //edgeList = build_MST(edgeList, n);
+    //vector<vector<pair<int,int>>> adjList = to_adj_list(edgeList, n);
+    
     cin >> l;
-
     while(l--){
         int u, v;
         cin >> u >> v;
-        cout << query(u-1, v-1, adjList) << endl;
+        cout << query(u-1, v-1) << endl;
     }
 
 }
