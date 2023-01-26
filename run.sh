0ca9117bb31d532225b82f02c0fffc417a5138b7
czartur <arturalves.ca@gmail.com>
2023-01-26 16:24:11 +0100

    new script

--- run.sh
+++ run.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/bash
+echo "version number ? (1,2 or 3)"
+read x
+echo "number of tests ? (less than 10 please)"
+read N
 
+echo "compiling itineraries_v$x.cpp ..."
+g++ -o executable itineraries_v$x.cpp MST.cpp
+
+export LC_NUMERIC="en_US.UTF-8"
+TIMEFORMAT=%R
+
+DIR="time/time_v$x.csv"
+echo "" >$DIR
 
 for i in input/*.in
 do
   i=$(basename $i)
   i=${i%".in"}
-  
   echo "testing $i..."
-  printf "time for $i:" >> time.txt
-  { time ./executable < input/$i".in" > output/$i".out" ;} 2>>time.txt
-  printf "\n" >> time.txt
+  echo -n "$i" >> $DIR
+  for run in $(seq 1 $N)
+  do
+    echo -n "," >> $DIR
+    duration=$( { time ./executable < input/$i".in" > output/$i".out" ;} 2>&1)
+    echo -n "$duration" >> $DIR
+    echo "$duration"
+  done
+  printf "\n" >> $DIR
 done
 
-echo "Done! Check time.txt for time report"
+rm executable
+
+echo "Done! Check $DIR for time report"
